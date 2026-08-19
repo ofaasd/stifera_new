5bfba1e937720fd5d7a0aad69aaf5cce3a0209d9
stiferasemarang <stiferasemarang@gmail.com>
2026-08-19 14:13:52 +0700

    Perbaikan favicon sedikit

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,7 +7,8 @@
 <title><?php echo !empty(config('dz.pagelevel.'.$CurrentPage.'.title')) ? config('dz.pagelevel.'.$CurrentPage.'.title').' | ' : '' ; echo config('dz.site_level.site_title'); ?></title>
 
 @include('elements.meta', ['CurrentPage' => $CurrentPage])
-<link rel="shortcut icon" type="image/png" href="{{ config('dz.site_level.favicon') }}">
+<link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
+<link rel="shortcut icon" type="image/png" href="{{ asset('images/favicon.png') }}">
 
 
 @include('elements.page-css', ['CurrentPage' => $CurrentPage])
